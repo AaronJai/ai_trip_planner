cbc1ec28a18688e4b97cb5a21d9c99c067f827d8
Aaron <aaron.tan1@gmail.com>
2024-08-09 00:01:00 +0800

    feat: implement create-trip page

--- app/(root)/create-trip/page.tsx
+++ app/(root)/create-trip/page.tsx
@@ -1,19 +1,104 @@
-import React from 'react'
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { SelectBudgetOptions, SelectTravelList } from '@/constants';
+import React, { useEffect, useState } from 'react'
+import ReactGoogleAutocomplete from 'react-google-autocomplete'
 
 const CreateTrip = () => {
+    const [place, setPlace] = useState();
+
+    const [formData, setFormData] = useState({});
+
+    const handleInputChange = (name: string, value: any) => {
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData])
+
     return (
-        <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+        <div className='max-w-4xl mx-auto sm:px-10 px-5 mt-10'>
             <h2 className='font-bold text-3xl'>
                 Tell us your travel preferences
             </h2>
             <p className='mt-3 text-gray-500 text-xl'>
-                Provide us some basic information, and our trip planner will curate a customised iinerary based on your preferences.
+                Provide us some basic information, and our trip planner will curate a customised itinerary based on your preferences.
             </p>
 
-            <div className='mt-20'>
+            <div className='mt-16 flex flex-col gap-9'>
                 <div>
-                    <h2 className='text-xl my-3 font-medium'>✈️ What is your destination? </h2>
+                    <h2 className='text-xl my-3 font-medium'>
+                        ✈️ What is your destination? 
+                    </h2>
+
+                    <ReactGoogleAutocomplete
+                        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+                        onPlaceSelected={(place) => {
+                            setPlace(place);
+                            handleInputChange('location', place);
+                        }}
+                        className='location_input'
+                    />
                 </div>
+
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>
+                        📅 How many days? 
+                    </h2>
+                    <Input placeholder='Ex. 3' type='number' min={1} onChange={(e) => handleInputChange('noOfDays', e.target.value)}/>
+                </div>
+
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>
+                        🏧 What is your Budget? 
+                    </h2>
+
+                    <div className='grid grid-cols-3 gap-5 mt-5'>
+                        {SelectBudgetOptions.map((item, index) => (
+                            <div 
+                                key={index} 
+                                className={`p-4 border cursor-pointer rounded-lg hover:shadow-lg ${formData?.budget === item.title && 'shadow-lg border-black border-2'}`}
+                                onClick={() => handleInputChange('budget', item.title)}
+                            >
+                                <h2 className='text-4xl'>{item.icon}</h2>
+                                <h2 className='font-bold text-lg'>{item.title}</h2>
+                                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>
+                        🤔 Who do you plan on travelling with on your next adventure? 
+                    </h2>
+
+                    <div className='grid grid-cols-4 gap-5 mt-5'>
+                        {SelectTravelList.map((item, index) => (
+                            <div 
+                                key={index} 
+                                className={`p-4 border cursor-pointer rounded-lg hover:shadow-lg ${formData?.people === item.people && 'shadow-lg border-black border-2'}`}
+                                onClick={() => handleInputChange('people', item.people)}
+                            >
+                                <h2 className='text-4xl'>{item.icon}</h2>
+                                <h2 className='font-bold text-lg'>{item.title}</h2>
+                                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+            </div>
+            
+            <div className='my-10 justify-end flex'>
+                <Button className=''>Generate Trip</Button>
+
             </div>
         </div>
     )
--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,54 @@
-export const headerLinks = [
-    
+export const SelectBudgetOptions = [
+    {
+        id: 1,
+        title: 'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: '💸',
+        budget: 'Low'
+    },
+    {
+        id: 2,
+        title: 'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: '💰',
+        budget: 'Mid'
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: "Don't worry about cost",
+        icon: '💎',
+        budget: 'High'
+    }
+]
+
+export const SelectTravelList = [
+    {
+        id: 1,
+        title: 'Just Me',
+        desc: 'I am travelling solo',
+        icon: '✈︎',
+        people: '1'
+    },
+    {
+        id: 2,
+        title: 'A Couple',
+        desc: 'I am travelling with my partner',
+        icon: '👫',
+        people: '2'
+    },
+    {
+        id: 3,
+        title: 'Family',
+        desc: 'I am travelling with my family',
+        icon: '👨‍👩‍👧‍👦',
+        people: '3+'
+    },
+    {
+        id: 4,
+        title: 'Friends',
+        desc: 'I am travelling with my friends',
+        icon: '👯‍♂️',
+        people: '4+'
+    }
 ]
